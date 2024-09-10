a847e3fb4feadfe7f06e7b4798fa78d01649f116
Denis Korolevkii <diniskorolevkij@gmail.com>
2024-09-10 12:55:26 +0600

    Создал проект.

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Денис Королевский on 10/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
